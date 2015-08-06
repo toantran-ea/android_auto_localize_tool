adaef21e63d1e8271a5e53c89f0cbcf7e83dea17
Toan.Tran+Thao.Hoang <toan.tran+thao.hoang@eastagile.com>
2015-08-06 18:47:37 +0700

    Supporting filtering out unable-to-translate items.

--- localize_tool.py
+++ localize_tool.py
@@ -8,13 +8,12 @@ import xml.etree.ElementTree as ET
 from xml.dom.minidom import parseString
 
 EXCEPTION_LIST = ('°',)
-CHECK_LIST = ('aircon_wind_speed_adjust',)
 
 def load_strings_file():
 	tree = ET.parse('strings.xml')
 	root = tree.getroot()
 	translated_resources_root = ET.Element("resources")
-
+	untransalated_resources_root = ET.Element("resources")
 	for child in root:
 		if child.text is not None  and  (child.text.encode('utf-8') not in EXCEPTION_LIST):
 			try:
@@ -23,21 +22,10 @@ def load_strings_file():
 				print name, '-->', translated_text
 				ET.SubElement(translated_resources_root, tag="string", name=name).text = translated_text
 			except:
-				print "can not translate text " + child.text.encode('utf-8')
-	xmlstr = parseString(ET.tostring(translated_resources_root)).toprettyxml(indent="   ")
+				print ">>>>>>>> can not translate text " + child.text.encode('utf-8')
+	xmlstr = parseString(ET.tostring(translated_resources_root, encoding="utf-8")).toprettyxml(indent="   ")
 	with open("a_strings.xml", "w") as f:
-		f.write(xmlstr.encode("UTF-8"))
-
-def check_item(item):
-	tree = ET.parse('strings.xml')
-	root = tree.getroot()
-	for child in root:
-		if child.text is not None  and  (child.text.encode('utf-8') not in EXCEPTION_LIST) and child.attrib['name'] in CHECK_LIST:
-			try:
-				translated_text = translate_to_english(child.text.encode('utf-8'))
-				print child.attrib['name'], '-->', translated_text
-			except:
-				print "can not translate text " + child.text.encode('utf-8')
+		f.write(xmlstr.encode("utf-8"))
 
 			
 
@@ -47,7 +35,3 @@ def translate_to_english(text):
 
 if __name__ ==  '__main__':
 	load_strings_file()
-	# for item in CHECK_LIST:
-	# 	check_item(item)
-
-	# print translate_to_english('风速调节')
