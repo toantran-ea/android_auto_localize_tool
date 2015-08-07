37437178e4eaffe0c2a5c237321a29c05d03b758
Toan Tran <me@toan.mobi>
2015-08-07 07:33:56 +0700

    Add script to handle updating translating API automagically.

---
+++ update.sh
@@ -0,0 +1,4 @@
+# use goslate as the internet auto-translating engine in this project
+# https://bitbucket.org/zhuoqiang/goslate
+
+curl -o goslate.py https://bitbucket.org/zhuoqiang/goslate/raw/tip/goslate.py
